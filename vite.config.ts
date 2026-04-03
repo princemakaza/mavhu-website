c7503b9da47575607be887d2db2df663de502355
princemakaza <zpmakaza@gmail.com>
2026-04-03 16:22:25 +0200

    jndjd

--- vite.config.ts
+++ vite.config.ts
@@ -6,15 +6,16 @@
 // export default defineConfig({
 //   plugins: [react(),tailwindcss()],
 // })
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
-    host: '0.0.0.0',
-    port: 5173
-  }
-})
+    host: "0.0.0.0", // allows access from any network interface
+    port: 5173,
+    allowedHosts: ["mavhu.com", "www.mavhu.com"], // ✅ add your domain here
+  },
+});
